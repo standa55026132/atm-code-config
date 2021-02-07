5c3fade5e80c55f85325159c966a6fa44e0e8a14
nastanda <nastanda1995@gmail.com>
2021-02-07 11:41:27 +0700

    initial commit

---
+++ src/main/java/atm/Bank.java
@@ -0,0 +1,42 @@
+package atm;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * A bank contains customers with bank accounts.
+ */
+public class Bank {
+
+   private String name;
+   private Map<Integer,Customer> customers;
+   private DataSourceFile dataSource;
+
+   /**
+    * Constructs a bank with no customers.
+    */
+   public Bank(String name) {
+      this.name = name;
+      this.dataSource = new DataSourceFile("customers.txt");
+      this.customers = dataSource.readCustomers();
+   }
+
+   /**
+    * Adds a customer to the bank.
+    * @param customer the customer to add
+    */
+   public void registerCustomer(Customer customer) {
+      customers.put(customer.getId(), customer);
+   }
+   
+   /** 
+    * Finds a customer in the bank.
+    * @param id a customer id
+    * @return the matching customer, or null if no customer
+    * matches
+    */
+   public Customer findCustomer(int id) {
+	  return customers.get(id);
+   }
+}
---
+++ src/main/java/atm/Main.java
@@ -0,0 +1,9 @@
+package atm;
+
+public class Main {
+    public static void main(String[] args) {
+        AtmUI atmUI = new AtmUI();
+        atmUI.run();
+        //System.out.println("Hello Sanfeng");
+    }
+}
